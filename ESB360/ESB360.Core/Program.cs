6f9a068e5eb8ea7b96708538666258b072c21ac8
he.jiafu <jasonhjfu@163.com>
2018-09-06 15:45:04 +0800

    init

---
+++ ESB360/ESB360.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ESB360.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
