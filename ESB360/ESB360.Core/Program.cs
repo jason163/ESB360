04f94aba93019ffafe5d0b629fc40032f1c78bca
he.jiafu <jasonhjfu@163.com>
2018-09-06 15:52:16 +0800

    test

--- ESB360/ESB360.Core/Program.cs
+++ ESB360/ESB360.Core/Program.cs
@@ -6,7 +6,7 @@ namespace ESB360.Core
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Baby!");
         }
     }
 }
